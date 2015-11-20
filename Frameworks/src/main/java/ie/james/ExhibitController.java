32dd9ebc2cc90c667c715c76f28990c69cadf969
User <jwalsh5@mycit.ie>
2015-11-20 13:28:38 +0000

    read exhibit from json, display on webpage

--- Frameworks/src/main/java/ie/james/ExhibitController.java
+++ Frameworks/src/main/java/ie/james/ExhibitController.java
@@ -8,17 +8,16 @@ import utility.FileParser;
 import java.io.IOException;
 import java.util.List;
 
-
 @RestController
 public class ExhibitController {
+
     @RequestMapping("/exhibit")
-    public Exhibit exhibit(@RequestParam(value="id") String id) throws IOException {
-        List<Exhibit> exhibits=new FileParser().parseFromJson("C:\\Users\\User\\Desktop\\Frameworks\\682506");
-        for(Exhibit e: exhibits){
-            if(e.getId().equalsIgnoreCase(id)){
+    public Exhibit exhibit(@RequestParam(value = "id") String id) throws IOException {
+        List<Exhibit> exhibits = new FileParser().parseFromJson("C:\\Users\\User\\Desktop\\Frameworks\\682506");
+        for (Exhibit e : exhibits) {
+            if (e.getId().equalsIgnoreCase(id)) {
                 return e;
             }
-
         }
         return null;
     }
